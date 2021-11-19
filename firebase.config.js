722889131119872185f5d3987493aa96fc413774
Olawale O Samson <smsnmicheal@gmail.com>
2021-11-19 13:02:09 +0100

    removed config file

--- firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDuM2P0D63bnVPNufe7JlyOUIO4ZhGi7CY",
-    authDomain: "next-blog-94773.firebaseapp.com",
-    projectId: "next-blog-94773",
-    storageBucket: "next-blog-94773.appspot.com",
-    messagingSenderId: "629661676642",
-    appId: "1:629661676642:web:66b723049b07102e904d3b",
-    measurementId: "G-QRNFWTY92H"
-}
